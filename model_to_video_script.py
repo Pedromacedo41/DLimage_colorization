54fd548f1cc36a33f81bc95035a6799ba6918d63
Pedro Macedo <pedro.flores@polytechnique.fr>
2020-03-03 14:08:17 +0100

    Adding description

--- model_to_video_script.py
+++ model_to_video_script.py
@@ -1,10 +1,15 @@
 import argparse
 
 
+'''
+Script for making a colorful video from a legacy black and white one
+'''
+
 def parse_args():
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('--model_name', default="none", type=str)
-    parser.add_argument('--file', default="none", type=str)
+    parser.add_argument('--input_file', default="none", type=str)
+    parser.add_argument('--output_file', default="none", type=str)
 
     args = parser.parse_args()
     return args
