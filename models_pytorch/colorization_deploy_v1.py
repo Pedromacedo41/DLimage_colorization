6b2cf500f1f9c51bc366d7ad11644018755faea9
Pedro Macedo <pedro.flores@polytechnique.fr>
2020-03-03 17:08:20 +0100

    modelo conferido no numero de dimensoes espaciais e todos os demais parametros estao extamente de acordo com o modelo que esta no outro git

--- models_pytorch/colorization_deploy_v1.py
+++ models_pytorch/colorization_deploy_v1.py
@@ -13,7 +13,7 @@ class colorization_deploy_v1(nn.Module):
         super(colorization_deploy_v1, self).__init__()
         self.T= T
 
-        self.conv1_1 = nn.Conv2d(3,64, kernel_size=3, stride=1, padding=1)
+        self.conv1_1 = nn.Conv2d(1,64, kernel_size=3, stride=1, padding=1)
         self.conv1_2 = nn.Conv2d(64,64, kernel_size=3, stride=2, padding=1)
 
         self.conv2_1 = nn.Conv2d(64,128, kernel_size=3, stride=1, padding=1)
@@ -25,25 +25,25 @@ class colorization_deploy_v1(nn.Module):
 
         self.conv4_1 = nn.Conv2d(256,512, kernel_size=3, stride=1, padding=1)
         self.conv4_2 = nn.Conv2d(512,512, kernel_size=3, stride=1, padding=1)
-        self.conv4_3 = nn.Conv2d(512,512, kernel_size=3, stride=2, padding=1)
+        self.conv4_3 = nn.Conv2d(512,512, kernel_size=3, stride=1, padding=1)
 
-        self.conv5_1 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=1)
-        self.conv5_2 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=1)
-        self.conv5_3 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=1)
+        self.conv5_1 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=2)
+        self.conv5_2 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=2)
+        self.conv5_3 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=2)
 
-        self.conv6_1 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=1)
-        self.conv6_2 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=1)
-        self.conv6_3 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=1)
+        self.conv6_1 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=2)
+        self.conv6_2 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=2)
+        self.conv6_3 = nn.Conv2d(512,512, kernel_size=3, stride=1, dilation= 2, padding=2)
 
-        self.conv7_1 = nn.Conv2d(512,256, kernel_size=3, stride=1, padding=1)
-        self.conv7_2 = nn.Conv2d(256,256, kernel_size=3, stride=1, padding=1)
-        self.conv7_3 = nn.Conv2d(256,256, kernel_size=3, stride=1, padding=1)
+        self.conv7_1 = nn.Conv2d(512,512, kernel_size=3, stride=1, padding=1)
+        self.conv7_2 = nn.Conv2d(512,512, kernel_size=3, stride=1, padding=1)
+        self.conv7_3 = nn.Conv2d(512,512, kernel_size=3, stride=1, padding=1)
 
-        self.conv8_1 = nn.ConvTranspose2d(256,128, kernel_size=1, stride=2, padding=1)
-        self.conv8_2 = nn.Conv2d(128,128, kernel_size=3, stride=1, padding=1)
-        self.conv8_3 = nn.Conv2d(128,128, kernel_size=3, stride=1, padding=1)
+        self.conv8_1 = nn.ConvTranspose2d(512,256, kernel_size=4, stride=2, padding=1)
+        self.conv8_2 = nn.Conv2d(256,256, kernel_size=3, stride=1, padding=1)
+        self.conv8_3 = nn.Conv2d(256,256, kernel_size=3, stride=1, padding=1)
 
-        self.conv8_313 = nn.Conv2d(128,313, kernel_size=1, stride=1, padding=1)
+        self.conv8_313 = nn.Conv2d(256,313, kernel_size=1, stride=1)
 
         self.bn1 = nn.BatchNorm2d(64)
         self.bn2 = nn.BatchNorm2d(128)
@@ -51,7 +51,7 @@ class colorization_deploy_v1(nn.Module):
         self.bn4 = nn.BatchNorm2d(512)
         self.bn5 = nn.BatchNorm2d(512)
         self.bn6 = nn.BatchNorm2d(512)
-        self.bn7 = nn.BatchNorm2d(256)
+        self.bn7 = nn.BatchNorm2d(512)
 
 
         self.lin = nn.Sequential(
@@ -120,12 +120,14 @@ class colorization_deploy_v1(nn.Module):
        
 
     def forward(self, input):
-        return self.lin(input)
-        # return F.softmax(self.lin(input), dim=1)
+        #return self.lin(input)
+        return F.softmax(self.lin(input), dim=1)
+
+ 
 
  
 if __name__ == '__main__': 
     net = colorization_deploy_v1()
-    t = net(torch.ones([1,3,224,224]))
+    t = net(torch.ones([1,1,224,224]))
     # print(t)
     print(t.shape)
