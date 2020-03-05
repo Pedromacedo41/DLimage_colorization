5a7f4fec3ff54a0a2a07e2adfde600c18b4b553d
Pedro Macedo <pedro.flores@polytechnique.fr>
2020-03-05 18:00:14 +0100

    loss ok!

--- train.py
+++ train.py
@@ -33,17 +33,19 @@ def loss(input, img_ab):
 
     #d1 = nnenc.decode_points_mtx_nd(input)
     img_ab = np.ones(shape= (1,2,224,224))
-    imput = torch.ones([1,313,224,224])
+    imput = torch.ones([1,313,224,224], dtype = torch.float64)
+    gpu(imput)
 
-    d2 = torch.tensor(nnenc.encode_points_mtx_nd(img_ab))
-    print(d2.shape)
-
+    d2 = torch.tensor(nnenc.encode_points_mtx_nd(img_ab), dtype = torch.float64)
     # dimension 1 x 224 x 224
+    gpu(d2)
     weights = priors.compute(torch.ones([1,313,224,224]))
-    
-    print(imput.log_().shape)
+ 
+    z2 = torch.sum(-imput.log_().mul_(d2), dim=1)
+    z2.mul_(weights)
 
-    
+    return z2.sum()
+
     #print((nnenc.decode_points_mtx_nd(torch.ones([1,313,224,224]).numpy())).shape)
     #print(d2.shpae)
 
